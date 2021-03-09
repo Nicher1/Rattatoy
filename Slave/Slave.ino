d6ef1a99446e4190d36503247a9091237ac6fcd0
Guffe99 <54105735+Guffe99@users.noreply.github.com>
2021-03-09 13:55:02 +0100

    SlaveStart
    Start slave code

--- Slave/Slave.ino
+++ Slave/Slave.ino
@@ -2,6 +2,16 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <Servo.h>
+Servo PUSH;
+
+#define PUSHDefault 90
+#define PUSHNow 20
+
+#define BUZZER 7
+
+int lysSensorPin = A0;
+int lysSensorValue = 0;
 
 //create an RF24 object
 RF24 radio(9, 8);  // CE, CSN
@@ -9,21 +19,31 @@ RF24 radio(9, 8);  // CE, CSN
 //address through which two modules communicate.
 const byte address[6] = "00001";
 
-void setup()
-{
+void setup(){
+  pinMode(BUZZER, OUTPUT);
+  digitalWrite(BUZZER,LOW);
   radio.begin();
+  Serial.begin(9600);
   
-  //set the address
-  radio.openWritingPipe(address);
+  PUSH.attach(6);
+  PUSH.write(PUSHDefault);
   
-  //Set module as transmitter
-  radio.stopListening();
+  radio.openWritingPipe(address);    //set the address
+  radio.stopListening();             //Set module as transmitter
 }
-void loop()
-{
-  //Send message to receiver
-  const char text[] = "Hello World";
-  radio.write(&text, sizeof(text));
+void loop(){
+digitalWrite(BUZZER, LOW);
+lysSensorValue = analogRead(lysSensorPin);
   
-  delay(1000);
+if (lysSensorValue <=750){
+  //Send message to receiver
+  //const char text[] = "Hello World";
+  //radio.write(&text, sizeof(text));
+  PUSH.write(PUSHNow);
+  digitalWrite(BUZZER, HIGH);
+}
+if (lysSensorValue > 750){
+  PUSH.write(PUSHDefault);
+}
+Serial.println(lysSensorValue);
 }
