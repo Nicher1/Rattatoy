ff0c2f04146fc65e69bf0259a141034ada8c5be1
Nichlas Overgaard Laugesen <nichlasost@gmail.com>
2021-03-09 13:49:24 +0100

    Stepper try 1

--- Master/Master.ino
+++ Master/Master.ino
@@ -2,7 +2,17 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <Stepper.h> // Include the header file
 
+// change this to the number of steps on your motor
+#define STEPS 32
+#define step1 6
+#define step2 7
+
+// create an instance of the stepper class using the steps and pins
+Stepper stepper(STEPS, 3, 5, 4, 6);
+
+int val = 0;
 //create an RF24 object
 RF24 radio(9, 8);  // CE, CSN
 
@@ -13,12 +23,16 @@ void setup()
 {
   while (!Serial);
     Serial.begin(9600);
-  
+  stepper.setSpeed(200);
   radio.begin();
-  
+  pinMode(step1, OUTPUT);
+  pinMode(step2, OUTPUT);
+  digitalWrite(step1, LOW);
+  digitalWrite(step2, LOW);
+
   //set the address
   radio.openReadingPipe(0, address);
-  
+
   //Set module as receiver
   radio.startListening();
 }
@@ -32,4 +46,14 @@ void loop()
     radio.read(&text, sizeof(text));
     Serial.println(text);
   }
+  if (Serial.available()>0)
+  {
+    val = Serial.parseInt();
+    stepper.step(val);
+    Serial.println(val); //for debugging
+  }
+  delay(1);
+  digitalWrite(step1, HIGH);
+  delay(1);
+  digitalWrite(step1, LOW);
 }
