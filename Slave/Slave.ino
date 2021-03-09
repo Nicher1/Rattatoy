9cff9eb727507f663dcc1d23cf1659c73cd57845
Guffe99 <54105735+Guffe99@users.noreply.github.com>
2021-03-09 14:35:20 +0100

    LysSensor
    Add LDR

--- Slave/Slave.ino
+++ Slave/Slave.ino
@@ -9,9 +9,11 @@ Servo PUSH;
 #define PUSHNow 20
 
 #define BUZZER 7
+#define GreenLED 5
 
 int lysSensorPin = A0;
 int lysSensorValue = 0;
+int lysLimit = 650;
 
 //create an RF24 object
 RF24 radio(9, 8);  // CE, CSN
@@ -20,29 +22,30 @@ RF24 radio(9, 8);  // CE, CSN
 const byte address[6] = "00001";
 
 void setup(){
-  pinMode(BUZZER, OUTPUT);
-  digitalWrite(BUZZER,LOW);
+  pinMode(BUZZER,   OUTPUT);
+  pinMode(GreenLED, OUTPUT);
   radio.begin();
   Serial.begin(9600);
   
   PUSH.attach(6);
   PUSH.write(PUSHDefault);
   
-  radio.openWritingPipe(address);    //set the address
-  radio.stopListening();             //Set module as transmitter
 }
 void loop(){
 digitalWrite(BUZZER, LOW);
+digitalWrite(GreenLED, LOW);
 lysSensorValue = analogRead(lysSensorPin);
   
-if (lysSensorValue <=750){
+if (lysSensorValue <= lysLimit){
   //Send message to receiver
-  //const char text[] = "Hello World";
-  //radio.write(&text, sizeof(text));
+  radio.openWritingPipe(address);    //set the address
+  radio.write(&lysSensorValue, sizeof(lysSensorValue));
+  radio.stopListening();             //Set module as transmitter
   PUSH.write(PUSHNow);
   digitalWrite(BUZZER, HIGH);
+  digitalWrite(GreenLED, HIGH);
 }
-if (lysSensorValue > 750){
+if (lysSensorValue > lysLimit){
   PUSH.write(PUSHDefault);
 }
 Serial.println(lysSensorValue);
