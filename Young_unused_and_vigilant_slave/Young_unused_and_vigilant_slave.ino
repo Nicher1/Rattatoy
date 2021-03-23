c936e37f7b4a7b5b9d22f9b20d3f586c0b3d91c5
Guffe99 <54105735+Guffe99@users.noreply.github.com>
2021-03-23 13:14:14 +0100

    Cleaning Slave
    Clean the slave

--- Young_unused_and_vigilant_slave/Young_unused_and_vigilant_slave.ino
+++ Young_unused_and_vigilant_slave/Young_unused_and_vigilant_slave.ino
@@ -5,16 +5,20 @@
 #include <Servo.h>
 Servo PUSH;
 
+//ServoValues
 #define PUSHDefault 90
 #define PUSHNow 20
 
+//Sensory output pins
 #define BUZZER 3   //PIN 4 NÅR VI ER DONE!!!!!!!!
 #define GreenLED 5
 
+//Values for timing
 int timer  = 0;
 int timer2 = 0;
 int period = 250;
 
+//Light sesor pins & values
 int lysSensorPin = A0;
 int lysSensorValue = 0;
 int lysLimit = 650;
@@ -28,12 +32,15 @@ const byte address[6] = "00001";
 void setup(){
   pinMode(BUZZER,   OUTPUT);
   pinMode(GreenLED, OUTPUT);
+  
   radio.begin();
+  
   Serial.begin(9600);
   
   PUSH.attach(6);
   PUSH.write(PUSHDefault);
 }
+
 void loop(){
   lysSensorValue = analogRead(lysSensorPin);
   radio.openWritingPipe(address);    //set the address  
