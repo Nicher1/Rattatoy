249c62e85b4b038772f416f73c27f242c33c7d00
Guffe99 <54105735+Guffe99@users.noreply.github.com>
2021-04-19 14:43:17 +0200

    Add radio code
    Write radio code

--- Slave/Slave.ino
+++ Slave/Slave.ino
@@ -1,3 +1,7 @@
+//Include Libraries
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
 #include <AccelStepper.h>
 
 // Define a stepper and the pins it will use
@@ -5,14 +9,20 @@ AccelStepper stepper(AccelStepper::DRIVER, 2, 3);
 
 float pos = 133.3333;
 
-void setup()
-{ 
+const byte address[6] = "69420";
+RF24 radio(7, 8);  // CE, CSN
+
+void setup(){
+  Serial.bein(9600);
+  radio.begin();
+   
   stepper.setMaxSpeed(1000);
   stepper.setAcceleration(1000);
 }
 
 void loop()
 {
+  radio.openReadingPipe(address);
   if (stepper.distanceToGo() == 0)
   {
     delay(500);
@@ -20,4 +30,5 @@ void loop()
     stepper.moveTo(pos);\
   }
   stepper.run();
+  radio.stopListening();
 }
